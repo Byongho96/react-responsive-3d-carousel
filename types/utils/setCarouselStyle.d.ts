ae47cd1ca928f4ba9406eb88e4a630466ce78e15
Byongho96 <unlike96@gmail.com>
2023-09-06 15:29:38 +0900

    Updates

--- types/utils/setCarouselStyle.d.ts
+++ types/utils/setCarouselStyle.d.ts
@@ -1,5 +1,11 @@
 /**
  *
+ * @param curIndex Index of the current carousel item
+ * @param carouselItems Collection of carousel items
+ */
+export declare function setOneCarouselStyle(curIndex: number, carouselItems: HTMLCollection): void;
+/**
+ *
  * @param length The total length of carousel items
  * @param curIndex Index of the current carousel item
  * @param width Width of a current carousel item
