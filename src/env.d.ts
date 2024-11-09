8b39cbd8f380c609a23894fdb391a468395f5b1a
Byongho <unlike96@gmail.com>
2024-11-09 14:51:07 +0900

    chore: add env.d.ts file for TypeScript environment definitions

---
+++ src/env.d.ts
@@ -0,0 +1,24 @@
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*webp' {
+  const src: string
+  export default src
+}
+
+declare module '*mp4' {
+  const src: string
+  export default src
+}
+
+declare module '*webm' {
+  const src: string
+  export default src
+}
