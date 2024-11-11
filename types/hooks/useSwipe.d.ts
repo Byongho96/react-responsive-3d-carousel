929c61ff0c0103a715052cfbcf2e6362a6cbaf4c
Byongho96 <Byongho96@users.noreply.github.com>
2024-11-11 02:44:12 +0000

    deploy: b563caff32d65470c5344f354696027bba147fdb

--- types/hooks/useSwipe.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Executes the corresponding function according to the direction of the touch swipe
- * @param containerRef HTML elements to detect touch events
- * @param onSwipeLeft Function to run when swiping to the left
- * @param onSwipeRight Function to run when swiping to the right
- */
-declare const useSwipe: (containerRef: React.RefObject<HTMLDivElement>, onSwipeLeft: () => void, onSwipeRight: () => void) => void;
-export default useSwipe;
