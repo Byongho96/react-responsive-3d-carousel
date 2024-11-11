3253288be8a7d3a5e7cc7efcb9098e609f345cab
Byongho96 <Byongho96@users.noreply.github.com>
2024-11-11 03:50:41 +0000

    deploy: d0b5992e3d4669b0e1619cecddffa77f08065fe3

---
+++ storybook/assets/Status.stories-08bf019b.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-78eee2bc.js";import{S as o}from"./Status-6fd31166.js";import"./index-8b3efc3f.js";import"./_commonjsHelpers-de833af9.js";const d={title:"Components/Status",component:o,argTypes:{length:{control:{type:"number",min:1}},curIndex:{table:{disable:!0}}},decorators:[a=>t("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",width:"100%",height:"100vh"},children:t(a,{})})]},e={args:{length:5,curIndex:0}};var r,n,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    length: 5,
+    curIndex: 0
+  }
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,d as default};
