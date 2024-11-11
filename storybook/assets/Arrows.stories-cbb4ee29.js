1ea6e9fee5aaa40f494b2435c1f5ba428bdc0127
Byongho96 <Byongho96@users.noreply.github.com>
2024-11-11 03:33:09 +0000

    deploy: 5572c3656faa566560e01bb59e25c27b4a490cd7

---
+++ storybook/assets/Arrows.stories-cbb4ee29.js
@@ -0,0 +1,25 @@
+import{j as e,a as t}from"./jsx-runtime-78eee2bc.js";import{A as m}from"./Arrows-a32ca28d.js";import"./index-8b3efc3f.js";import"./_commonjsHelpers-de833af9.js";const w={title:"Components/Arrows",component:m,args:{},argTypes:{onClickNext:{table:{disable:!0}},onClickPrev:{table:{disable:!0}},prevIcon:{table:{disable:!0}},nextIcon:{table:{disable:!0}}},decorators:[g=>e("div",{style:{display:"flex",alignItems:"center",height:"100vh"},children:e(g,{})})]},r={args:{}},o={args:{...r.args,prevArrowTranslate:["100px","-100px"],nextArrowTranslate:["-100px","100px"]}},n={args:{...r.args,width:"3rem",height:"3rem",prevIcon:t("svg",{width:"24",height:"24",viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",children:[e("circle",{cx:"12",cy:"12",r:"10"}),e("polyline",{points:"12 8 8 12 12 16"}),e("line",{x1:"16",y1:"12",x2:"8",y2:"12"})]}),nextIcon:t("svg",{width:"24",height:"24",viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",children:[e("circle",{cx:"12",cy:"12",r:"10"}),e("polyline",{points:"12 16 16 12 12 8"}),e("line",{x1:"8",y1:"12",x2:"16",y2:"12"})]})}};var s,i,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(a=(i=r.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var l,c,p;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    prevArrowTranslate: ['100px', '-100px'],
+    nextArrowTranslate: ['-100px', '100px']
+  }
+}`,...(p=(c=o.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var d,x,u;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    width: '3rem',
+    height: '3rem',
+    prevIcon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <circle cx="12" cy="12" r="10"></circle>
+        <polyline points="12 8 8 12 12 16"></polyline>
+        <line x1="16" y1="12" x2="8" y2="12"></line>
+      </svg>,
+    nextIcon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <circle cx="12" cy="12" r="10"></circle>
+        <polyline points="12 16 16 12 12 8"></polyline>
+        <line x1="8" y1="12" x2="16" y2="12"></line>
+      </svg>
+  }
+}`,...(u=(x=n.parameters)==null?void 0:x.docs)==null?void 0:u.source}}};const f=["Default","Translate","CustomIcon"];export{n as CustomIcon,r as Default,o as Translate,f as __namedExportsOrder,w as default};
